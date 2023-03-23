39dcc2f6faec0613e1325e1b2f0ad4c6090168b2
foxstudent103280 <dCu88WVcucnA>
2023-03-23 15:27:54 +0200

    Fixed XML issue with visibility elements on Fragments

--- app/src/main/java/com/example/task3_benchmarks/FragmentCollections.java
+++ app/src/main/java/com/example/task3_benchmarks/FragmentCollections.java
@@ -32,7 +32,7 @@ public class FragmentCollections extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        buttonStart = binding.buttonStart;
+        buttonStart = binding.buttonStartFragmens;
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
